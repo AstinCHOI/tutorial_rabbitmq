2485c2d02d807580e44190cfe1991ce92229469a
astinchoi <asciineo@gmail.com>
2018-06-05 18:07:53 +0900

    all tutorials

---
+++ 3_pub_sub/emit_log.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import pika
+import sys
+
+'''
+$ sudo rabbitmqctl list_exchanges
+$ rabbitmqctl list_bindings
+'''
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs',
+                         exchange_type='fanout')
+
+message = ' '.join(sys.argv[1:]) or "info: Hello World!"
+channel.basic_publish(exchange='logs',
+                      routing_key='',
+                      body=message)
+print(" [x] Sent %r" % message)
+connection.close()
